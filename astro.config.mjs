fd887064dfb9dd1e7cd3b367f8129304d4f29973
Brad Beltowski <bradbeltowskigmail.com>
2026-04-03 23:33:00 -0400

    Solid blog starting point.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import bradDevTheme from './src/styles/brad-dev-theme.json';
 
 export default defineConfig({
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
-      theme: 'one-dark-pro',
+      theme: bradDevTheme,
       wrap: false,
     },
   },
